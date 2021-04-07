bb33344a0cabdb820ff48c8a83ea45462ab011a5
Muthuramakrishnan Mahadevan <muthu.rama.krishnan96@gmail.com>
2021-04-07 19:29:17 +0530

    final commit

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -5,5 +5,5 @@ const { processPayment, sendStripeApi } = require('../controllers/paymentControl
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/payment/process').post(isAuthenticatedUser, processPayment);
-router.route('/stripeapi').get(isAuthenticatedUser, sendStripeApi);
+router.route('/stripeapi').get(sendStripeApi);
 module.exports = router;
